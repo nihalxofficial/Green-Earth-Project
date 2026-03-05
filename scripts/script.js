b2f9c11bb7cd53aa6fcb15f5352c4fe1ecc1a026
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-03-05 16:00:15 +0600

    All trees and spinner added

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
 
 const categoriesContainer = document.getElementById('categories-container')
+const allTreeBtn = document.getElementById("all-tree-btn");
+const treeContainer = document.getElementById("tree-container");
+const loadingSpinner = document.getElementById("loading-spinner");
+
+const showLoading = (status) => {
+    if(status){
+        loadingSpinner.classList.remove(("hidden"));
+    }else{
+        loadingSpinner.classList.add("hidden");
+    }
+}
+
 
 const loadCategories = async () => {
     // fetch('https://openapi.programming-hero.com/api/categories')
@@ -11,7 +23,7 @@ const loadCategories = async () => {
     const data = await res.json();
     
     data.categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
         const btn = document.createElement('button')
         btn.className = "btn justify-start bg-transparent w-full border-none";
         btn.innerText = category.category_name;
@@ -19,4 +31,41 @@ const loadCategories = async () => {
         
     });
 }
+
+
+
+const loadAllTrees = async () => {
+    showLoading(true);
+    const res = await fetch("https://openapi.programming-hero.com/api/plants")
+    const data = await res.json();
+    displayTrees(data.plants);
+    
+    
+}
+
+
+const displayTrees = (plants) => {    
+    treeContainer.innerHTML = "";
+    plants.forEach(plant => {
+        const card = document.createElement("div");
+        card.className = "card bg-white rounded-xl shadow-md p-3 space-y-2";
+        card.innerHTML = `
+        <div class="image ">
+            <img src="${plant.image}" alt="${plant.name}" class="w-full h-[200px] rounded-xl object-cover">
+            </div>
+            <h2 class="font-semibold">${plant.name}</h2>
+            <p class="text-sm text-gray-600 line-clamp-2">${plant.description}</p>
+            <div class="flex justify-between">
+                <button class="bg-green-300 text-green rounded-full px-3">${plant.category}</button>
+                <button class="font-semibold">৳${plant.price}</button>
+            </div>
+        <button class="btn bg-green-700 text-white py-2 rounded-full"><i class="fa-solid fa-cart-shopping"></i> Add to Cart</button>
+        `;
+
+    treeContainer.appendChild(card);
+    showLoading(false);
+    })
+}
+
 loadCategories();
+loadAllTrees();
